297282250dca441898f5ab6054d16010d119f834
Denis Guiraudet <guiraudet@live.fr>
2024-12-01 21:49:53 +0100

    2024-01

--- 2024-01/script.ts
+++ 2024-01/script.ts
@@ -2,4 +2,7 @@
 // Either sort both array
 // Or do it on the go with a sorting algo
 
+// Radix sort on binary of integer ?
+// https://en.m.wikipedia.org/wiki/Radix_sort
+// maybe do msd since the interger length dont change
 
